2d6a86b8bf50f65f4eba988a592acdcd831ca3b8
hyungtaik <hyungtaik@naver.com>
2019-08-15 03:19:36 +0900

    이미지 절대경로 업로드(캘린더 생성,수정시 이미지)

---
+++ Shalendar_Server/src/main/java/kr/co/MIND/image/ImageServiceImpl.java
@@ -0,0 +1,123 @@
+package kr.co.MIND.image;
+
+import javax.inject.Inject;
+import org.springframework.stereotype.Service;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.MediaType;
+import java.util.UUID;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.text.DecimalFormat;
+import java.util.Calendar;
+
+import javax.imageio.ImageIO;
+
+import org.imgscalr.Scalr;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.FileCopyUtils;
+
+@Service("ImageService")
+public class ImageServiceImpl implements ImageService {
+
+	@Inject
+	
+	private static Map<String, MediaType> mediaMap;
+
+	static{
+		mediaMap = new HashMap<String, MediaType>();		
+		mediaMap.put("JPG", MediaType.IMAGE_JPEG);
+		mediaMap.put("GIF", MediaType.IMAGE_GIF);
+		mediaMap.put("PNG", MediaType.IMAGE_PNG);
+	}
+
+	public MediaType getMediaType(String type){
+		return mediaMap.get(type.toUpperCase());
+	}
+
+	private static final Logger logger = LoggerFactory.getLogger(ImageServiceImpl.class);
+
+	public String uploadFile(String uploadPath, String originalName, byte[] fileData) throws Exception {
+
+		UUID uid = UUID.randomUUID();
+
+		String savedName = uid.toString() + "_" + originalName;
+
+		String savedPath = calcPath(uploadPath);
+
+		File target = new File(uploadPath + savedPath, savedName);
+
+		FileCopyUtils.copy(fileData, target);
+
+		String formatName = originalName.substring(originalName.lastIndexOf(".") + 1);
+
+		String uploadedFileName = null;
+
+		if (getMediaType(formatName) != null) {
+			uploadedFileName = makeThumbnail(uploadPath, savedPath, savedName);
+		} else {
+			uploadedFileName = makeIcon(uploadPath, savedPath, savedName);
+		}
+
+		return uploadedFileName;
+
+	}
+
+	public String makeIcon(String uploadPath, String path, String fileName) throws Exception {
+
+		String iconName = uploadPath + path + File.separator + fileName;
+
+		return iconName.substring(uploadPath.length()).replace(File.separatorChar, '/');
+	}
+
+	public String makeThumbnail(String uploadPath, String path, String fileName) throws Exception {
+
+		BufferedImage sourceImg = ImageIO.read(new File(uploadPath + path, fileName));
+
+		BufferedImage destImg = Scalr.resize(sourceImg, Scalr.Method.AUTOMATIC, Scalr.Mode.FIT_TO_HEIGHT, 100);
+
+		String thumbnailName = uploadPath + path + File.separator + "s_" + fileName;
+
+		File newFile = new File(thumbnailName);
+		String formatName = fileName.substring(fileName.lastIndexOf(".") + 1);
+
+		ImageIO.write(destImg, formatName.toUpperCase(), newFile);
+		return thumbnailName.substring(uploadPath.length()).replace(File.separatorChar, '/');
+	}
+
+	public String calcPath(String uploadPath) {
+
+		Calendar cal = Calendar.getInstance();
+
+		String yearPath = File.separator + cal.get(Calendar.YEAR);
+
+		String monthPath = yearPath + File.separator + new DecimalFormat("00").format(cal.get(Calendar.MONTH) + 1);
+
+		String datePath = monthPath + File.separator + new DecimalFormat("00").format(cal.get(Calendar.DATE));
+
+		makeDir(uploadPath, yearPath, monthPath, datePath);
+
+		logger.info(datePath);
+
+		return datePath;
+	}
+
+	public void makeDir(String uploadPath, String... paths) {
+
+		if (new File(paths[paths.length - 1]).exists()) {
+			return;
+		}
+
+		for (String path : paths) {
+
+			File dirPath = new File(uploadPath + path);
+
+			if (!dirPath.exists()) {
+				dirPath.mkdir();
+			}
+		}
+	}
+
+	
+}
